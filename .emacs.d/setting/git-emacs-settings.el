9a954ab0a55b93fc957a4d6d13ff4416218a87ec
HuHoo <ghoststears@gmail.com>
2010-12-29 16:03:25 +0800

    add git-emacs mode

---
+++ .emacs.d/setting/git-emacs-settings.el
@@ -0,0 +1,21 @@
+;; -*- Emacs-Lisp -*-
+;; -*- coding:utf-8 -*-
+;;
+;; ------------------------------------------------------------------------------
+;;
+;; @file        git-emacs-settings.org
+;; @brief
+;; @author      iHuHoo.W (ihuhoo.w@gmail.com)
+;; @date        2010-12-29 15:51:24
+;;
+;;     Copyright (C) 2010 iHuHoo.W, all rights reserved.
+;; 
+;; ------------------------------------------------------------------------------
+;;
+
+;; git-emacs setting
+
+(require 'git-emacs)
+
+(provide 'git-emacs-settings)
+
