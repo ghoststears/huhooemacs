826b08029bafd60a55c858903777a67980d87b80
iHuHoo.W <ihuhoo.w@gmail.com>
2012-04-01 17:44:41 +0800

    generic copy is done.

--- .emacs.d/my-lisp/generic-copy.el
+++ .emacs.d/my-lisp/generic-copy.el
@@ -5,6 +5,7 @@
 ;; http://emacser.com/non-programmer.htm
 ;;
 
+
 (defun get-point (symbol &optional arg)
   "get the point"
   (funcall symbol arg)
@@ -13,45 +14,38 @@
 
 (defun copy-thing (begin-of-thing end-of-thing &optional arg)
   "copy thing between beg & end into kill ring"
-  (let ((beg (get-point begin-of-thing 1))
-        (end (get-point end-of-thing arg)))
-    (copy-region-as-kill beg end))
-  )
-
-(defun paste-to-mark(&optional arg)
-  "Paste things to mark, or to the prompt in shell-mode"
-  (let ((pasteMe 
-         (lambda()
-           (if (string= "shell-mode" major-mode)
-               (progn (comint-next-prompt 25535) (yank))
-             (progn (goto-char (mark)) (yank) )))))
-    (if arg
-        (if (= arg 1)
-            nil
-          (funcall pasteMe))
-      (funcall pasteMe))
-    )
+  (save-excursion
+    (let ((beg (get-point begin-of-thing 1))
+          (end (get-point end-of-thing arg)))
+      (copy-region-as-kill beg end)))
   )
 
 (defun generic-copy-word (&optional arg)
   "Copy words at point into kill-ring"
   (interactive "P")
   (copy-thing 'backward-word 'forward-word arg)
-  (paste-to-mark arg)
   )
 
 (defun generic-copy-line (&optional arg)
   "Save current line into Kill-Ring without mark the line "
   (interactive "P")
   (copy-thing 'beginning-of-line 'end-of-line arg)
-  (paste-to-mark arg)
   )
+
+(defun generic-copy-whole-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring"
+  (interactive "p")
+  (kill-ring-save (line-beginning-position)
+                  (line-beginning-position (+ 1 arg)))
+  (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
+
+
+
 
 (defun generic-copy-paragraph (&optional arg)
   "Copy paragraphes at point"
   (interactive "P")
   (copy-thing 'backward-paragraph 'forward-paragraph arg)
-  (paste-to-mark arg)
   )
 
 (defun beginning-of-string(&optional arg)
@@ -59,6 +53,7 @@
   (re-search-backward "[ \t]" (line-beginning-position) 3 1)
   (if (looking-at "[\t ]")  (goto-char (+ (point) 1)) )
   )
+
 (defun end-of-string(&optional arg)
   " "
   (re-search-forward "[ \t]" (line-end-position) 3 arg)
@@ -70,7 +65,6 @@
      When used in shell-mode, it will paste string on shell prompt by default "
   (interactive "P")
   (copy-thing 'beginning-of-string 'end-of-string arg)
-  (paste-to-mark arg)
   )
 
 (defun beginning-of-parenthesis(&optional arg)
@@ -90,13 +84,13 @@
      When used in shell-mode, it will paste parenthesis on shell prompt by default "
   (interactive "P")
   (copy-thing 'beginning-of-parenthesis 'end-of-parenthesis arg)
-  (paste-to-mark arg)
   )
 
-(global-set-key (kbd "C-c l") 'generic-copy-line)
+(global-set-key (kbd "C-c l"  'generic-copy-whole-line)
+(global-set-key (kbd "C-c L") 'generic-copy-line)
 (global-set-key (kbd "C-c w") 'generic-copy-word)
+(global-set-key (kbd "C-c l") 'generic-copy-line)
 (global-set-key (kbd "C-c p") 'generic-copy-paragraph)
-(global-set-key (kbd "C-c s") 'generic-copy-string)
-(global-set-key (kbd "C-c a") 'generic-copy-paragraph)
+
 
 (provide 'generic-copy)
